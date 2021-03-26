3bb60a1a44b6f575968915307ab2f92bc89da820
Sacha Jungerman <jungerm2@illinois.edu>
2021-03-26 14:46:02 -0500

    gather defs + hash->uuid

--- SVGFuncAnimation.py
+++ SVGFuncAnimation.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 from io import StringIO
 
 from matplotlib.artist import Artist
+from matplotlib.text import Text
 
 
 # Javascript template for HTMLWriter
@@ -266,7 +267,10 @@ def get_all_children(artist):
 
 
 class SVGFuncAnimation:
-    GROUP_XPATH = './/*/{http://www.w3.org/2000/svg}g'
+    NAMESPACES = {
+        'svg': 'http://www.w3.org/2000/svg',
+        'xlink': 'http://www.w3.org/1999/xlink'
+    }
 
     def __init__(self, fig, func, frames, init_func=None, fargs=None, default_mode='loop', interval=200):
         self._fig = fig
@@ -280,6 +284,67 @@ class SVGFuncAnimation:
         self._base_document = None
         self._embed_frames = []
 
+    @staticmethod
+    def _tree_tostring(tree):
+        ET.register_namespace('', 'http://www.w3.org/2000/svg')
+        ET.register_namespace('xlink', 'http://www.w3.org/1999/xlink')
+        return ET.tostring(tree, encoding='unicode')
+
+    @staticmethod
+    def _recursive_remove(root, xpath, ns):
+        for child in root.findall(xpath, ns):
+            root.remove(child)
+
+        for child in list(root):
+            SVGFuncAnimation._recursive_remove(child, xpath, ns)
+
+    def _gather_defs(self):
+        # SVG definitions (i.e: <defs>) are generated on the fly by each artist if they
+        # haven't yet been created. Therefore we need to gather them and place them
+        # in the base document such that every frame has access to them.
+        # Note: This step could be improved. Currently it depends on python's builtin
+        # XML library which cannot handle XML chunks so we need to hack together a
+        # valid doc for it to parse, then remove the excess (see below).
+
+        # Find the first <defs> in the base document and add all defs to it
+        base_document_tree = ET.fromstring(self._base_document)
+        defs = []
+
+        # Gather all defs from the base document that isn't the main defs
+        for ds in base_document_tree.findall('.//*/svg:defs', self.NAMESPACES):
+            defs.append(ds)
+
+        # Purge all defs from main document
+        for child in list(base_document_tree):
+            self._recursive_remove(child, 'svg:defs', self.NAMESPACES)
+
+        # Now filter through the embedded frames and remove any defs in those
+        filtered_frames = []
+        start, end = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">', '</svg>'
+
+        for embedded_frame in self._embed_frames:
+            drawn_artists = {}
+            for gid, doc in embedded_frame.items():
+                # Make the doc a valid XML file, otherwise we can't parse it
+                doc = start + doc + end
+                doc = ET.fromstring(doc)
+                for ds in doc.findall('.//*/svg:defs', self.NAMESPACES):
+                    defs.append(ds)
+                self._recursive_remove(doc, 'svg:defs', self.NAMESPACES)
+                # We can't simply do _tree_tostring on the doc's (only) child, because
+                # the XML package will add namespace info to that child... It's annoying
+                drawn_artists[gid] = self._tree_tostring(doc).replace(start, '', 1).replace(end, '', 1)
+            filtered_frames.append(drawn_artists)
+        self._embed_frames = filtered_frames
+
+        # Add all defs to main_def, regenerate the base doc
+        main_defs = ET.SubElement(base_document_tree, 'defs')
+        for d in defs:
+            for child in list(d):
+                main_defs.append(child)
+
+        self._base_document = self._tree_tostring(base_document_tree)
+
     def grab_frames(self):
         # Clear previous data
         self._base_document = None
@@ -291,13 +356,15 @@ class SVGFuncAnimation:
             if self._init_func:
                 for artist in self._init_func():
                     self._fig.add_artist(artist)
+                    # artist.set_visible(True)
 
             # Set the gid of every artist to it's hash, the idea here is that
             # when an artist is drawn in SVG it will be encased in a group with
             # an id equal to the artist's gid and the hash of an artist doesn't
             # change when the artist's data changes.
             for artist in get_all_children(self._fig):
-                artist.set_gid(str(hash(artist)))
+                # artist.set_gid(str(hash(artist)))
+                artist.set_gid(uuid.uuid4().hex)
 
             # Now we can save the initial figure, this creates a cached
             # SVG rendered, and will produce our base SVG document
@@ -311,14 +378,15 @@ class SVGFuncAnimation:
             # because the SVG group associated with them won't be in the
             # base document so we won't be able to update it properly.
             tree = ET.fromstring(self._base_document)
-            known_groups = {g.get('id') for g in tree.findall(self.GROUP_XPATH)}
+            known_groups = {g.get('id') for g in tree.findall('.//*/svg:g', namespaces=self.NAMESPACES)}
 
             # Get all subsequent frames by only drawing
             # the artists returned by the user's func
             for framedata in self._iter_gen:
                 drawn_artists = {}
                 for artist in self._func(framedata, *self._args):
-                    artist_gid = str(hash(artist))
+                    # artist_gid = str(hash(artist))
+                    artist_gid = artist.get_gid()
 
                     # Check that this artist is known
                     if artist_gid not in known_groups:
@@ -333,6 +401,7 @@ class SVGFuncAnimation:
                     drawn_artist = f.read()
                     drawn_artists[artist_gid] = drawn_artist
                 self._embed_frames.append(drawn_artists)
+        self._gather_defs()
 
     def save(self, filename):
         if not self._base_document:
@@ -341,7 +410,7 @@ class SVGFuncAnimation:
         mode_dict = dict(once_checked='', loop_checked='', reflect_checked='')
         mode_dict[self._default_mode + '_checked'] = 'checked'
 
-        with open(filename, 'w') as of:
+        with open(filename, 'w', encoding='utf-8') as of:
             of.write(JS_INCLUDE + STYLE_INCLUDE)
             of.write(DISPLAY_TEMPLATE.format(id=uuid.uuid4().hex,
                                              Nframes=len(self._embed_frames),
